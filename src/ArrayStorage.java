58fd32cbf19c9319cc57f8178684ba0b6b6ba0c6
unknown <metsubysi@gmail.com>
2024-01-14 17:48:11 +0000

    14.01.24

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,13 +1,19 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
@@ -15,16 +21,27 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
+        size--;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                storage[i] = storage[size];
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] a = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            a[i] = storage[i];
+        }
+        return a;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
