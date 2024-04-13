ef0bb342754e80c4db0f09caa8fe9c4c26effe00
unknown <metsubysi@gmail.com>
2024-04-13 13:02:40 +0100

    13.04

---
+++ src/test/SuiteTestClass.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ArrayStorageTest.class,
+        SortedArrayStorageTest.class,
+        ListStorageTest.class,
+        MapStorageTest.class} )
+public class SuiteTestClass {
+}
