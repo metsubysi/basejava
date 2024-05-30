a8bee6f0ca3725b623f0fa48e564ac3588ba7c95
unknown <metsubysi@gmail.com>
2024-05-30 13:44:10 +0100

    Edited file: AbstractFileStorage

--- src/com/storage/AbstractFileStorage.java
+++ src/com/storage/AbstractFileStorage.java
@@ -1,37 +1,104 @@
 package com.storage;
 
+import com.exception.StorageException;
 import com.model.Resume;
 
-import java.io.*;
-import java.util.UUID;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+public abstract class AbstractFileStorage extends AbstractStorage<File> {
 
-public abstract class AbstractFileStorage {
+    private File directory;
 
-    public void saveResume(Resume resume) {
-        String fileName = generateFileName(resume.getUuid());
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
-            oos.writeObject(resume);
-            System.out.println("Resume saved to file: " + fileName);
+    protected AbstractFileStorage(File directory) {
+        Objects.requireNonNull(directory, "directory must not be null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not a directory");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not readable/writable");
+        }
+        this.directory = directory;
+    }
+
+    @Override
+    public void clear() {
+        File[] files = checkedListFiles();
+        for (File file : files) {
+            doDelete(file);
+        }
+    }
+
+    @Override
+    public int size() {
+        return checkedListFiles().length;
+    }
+
+    @Override
+    protected List<Resume> getAll() {
+        return Arrays.stream(checkedListFiles())
+                .map(this::doGet)
+                .collect(Collectors.toList());
+    }
+
+    private File[] checkedListFiles() {
+        File[] files = directory.listFiles();
+        if (files == null) {
+            throw new StorageException("I/O error", "");
+        }
+        return files;
+    }
+
+    @Override
+    protected File getSearchKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    protected void doUpdate(Resume r, File searchKey) {
+
+        try {
+            doWrite(r, searchKey);
         } catch (IOException e) {
-            System.err.println("Error saving resume to file: " + e.getMessage());
+            throw new StorageException("I/O error", searchKey.getName());
         }
     }
 
-    public Resume loadResume(String uuid) {
-        String fileName = generateFileName(uuid);
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
-            Resume resume = (Resume) ois.readObject();
-            System.out.println("Resume loaded from file: " + fileName);
-            return resume;
-        } catch (IOException | ClassNotFoundException e) {
-            System.err.println("Error loading resume from file: " + e.getMessage());
-            return null;
+    @Override
+    protected boolean isExist(File file) {
+        return file.exists();
+    }
+
+    @Override
+    protected void doSave(Resume r, File file) {
+        try {
+            file.createNewFile();
+            doWrite(r, file);
+        } catch (IOException e) {
+            throw new StorageException("I/O error", file.getName());
         }
     }
 
-    private String generateFileName(String uuid) {
-        return uuid.toString() + ".txt";
+    @Override
+    protected Resume doGet(File file) {
+        try {
+            return doRead(file);
+        } catch (IOException e) {
+            throw new StorageException("I/O error", file.getName());
+        }
     }
 
-    public abstract void deleteResume(UUID uuid);
+    @Override
+    protected void doDelete(File file) {
+        if (!file.delete()) {
+            throw new StorageException("Failed to delete file", file.getName());
+        }
+    }
+
+    protected abstract void doWrite(Resume r, File file) throws IOException;
+
+    protected abstract Resume doRead(File file) throws IOException;
 }
