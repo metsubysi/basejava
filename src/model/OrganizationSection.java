a55e332858e28b05ca5572a3897336d4db7da873
PC <metsubysi@gmail.com>
2026-03-17 14:59:46 +0000

    lesson 7 (17.03.2026)

---
+++ src/model/OrganizationSection.java
@@ -0,0 +1,4 @@
+package model;
+
+public class OrganisationSection extends Section {
+}
---
+++ test/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,148 @@
+package storage;
+
+import model.Resume;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+import Exception.NotExistStorageException;
+import Exception.ExistStorageException;
+import Exception.StorageException;
+
+import java.util.List;
+
+public abstract class AbstractStorageTest {
+
+    protected AbstractStorageTest(Storage storage) {
+        this.storage = storage;
+    }
+
+    public Storage storage;
+    private static final String UUID_1 = "Дмитрий Соколов";
+    private static final String UUID_2 = "Анна Смирнова";
+    private static final String UUID_3 = "Сергей Волков";
+    private static final String UUID_4 = "Екатерина Новикова";
+    private static final String UUID_5 = "Алексей Захаров";
+    private static final String UUID_6 = "Мария Ильина";
+    private static final Resume R1;
+    private static final Resume R2;
+    private static final Resume R3;
+    private static final Resume R4;
+    private static final Resume R5;
+    private static final Resume R6;
+
+    static {
+            R1 = new Resume(UUID_1);
+            R2 = new Resume(UUID_2);
+            R3 = new Resume(UUID_3);
+            R4 = new Resume(UUID_4);
+            R5 = new Resume(UUID_5);
+            R6 = new Resume(UUID_6);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        storage.clear();
+        storage.save(R1);
+        storage.save(R2);
+        storage.save(R3);
+        storage.save(R4);
+        storage.save(R5);
+    }
+
+    @org.junit.Test
+    public void clear() throws Exception {
+        storage.clear();
+        assertSize(0);
+    }
+
+    @org.junit.Test
+    public void update() throws Exception {
+        Resume newResume = new Resume(UUID_1);
+        storage.update(newResume);
+        assertTrue(newResume == storage.get(UUID_1));
+    }
+
+    @org.junit.Test
+    public void getAll() throws Exception {
+        List<Resume> array = storage.getAllSorted();
+        assertEquals(5, array.size());
+        assertEquals(R1, array.get(0));
+        assertEquals(R2, array.get(1));
+        assertEquals(R3, array.get(2));
+        assertEquals(R4, array.get(3));
+        assertEquals(R5, array.get(4));
+    }
+
+    @org.junit.Test
+    public void get() throws Exception {
+        assertGet(R1);
+        assertGet(R2);
+        assertGet(R3);
+        assertGet(R4);
+        assertGet(R5);
+    }
+
+    @org.junit.Test
+    public void save() throws Exception {
+        storage.save(R6);
+        assertSize(6);
+        assertGet(R6);
+    }
+
+    @org.junit.Test (expected = NotExistStorageException.class)
+    public void delete() throws Exception{
+        storage.delete(UUID_4);
+        assertSize(4);
+        storage.get(UUID_4);
+    }
+
+    @org.junit.Test(expected = NotExistStorageException.class)
+    public void deleteNotExist() throws Exception{
+        // Пытаемся удалить несуществующий UUID (который выдаст индекс -1)
+        storage.delete("DUMMY_UUID_THAT_DOES_NOT_EXIST");
+    }
+
+    @org.junit.Test(expected = NotExistStorageException.class)
+    public void updateNotExist() throws Exception{
+        // Пытаемся удалить несуществующий UUID (который выдаст индекс -1)
+        storage.update(storage.get("DUMMY_UUID_THAT_DOES_NOT_EXIST"));
+    }
+
+    @org.junit.Test(expected = ExistStorageException.class)
+    public void saveExist() throws Exception{
+        // Пытаемся удалить несуществующий UUID (который выдаст индекс -1)
+        storage.save(new Resume(UUID_2));
+    }
+
+    @org.junit.Test
+    public void size() {
+        assertSize(5);
+    }
+
+    @org.junit.Test(expected = NotExistStorageException.class)
+    public void getNotExist() throws Exception {
+        storage.get("dummy");
+    }
+
+    @org.junit.Test(expected = StorageException.class)
+    public void saveOverflow() throws Exception {
+        try {
+            for (int i = 5; i < AbstractArrayStorage.STORAGE_LIMIT; i++) {
+                storage.save(new Resume("Максим Морозов" + i));
+            }
+        } catch (Exception e) {
+            Assert.fail();
+        }
+        storage.save(new Resume("Максим Морозов"));
+    }
+
+    private void assertSize(int size) {
+        assertEquals(size, storage.size());
+    }
+    private void assertGet(Resume r) {
+        assertEquals(r, storage.get(r.getUuid()));
+    }
+}
